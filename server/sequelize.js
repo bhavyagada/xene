8a6abb9e2d7fc3a6c85891626ab49acaf0093c9a
bhavya <bsg@somaiya.edu>
2025-03-08 15:29:08 -0800

    set pg ssl mode

--- server/sequelize.js
+++ server/sequelize.js
@@ -10,7 +10,13 @@ const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   dialect: process.env.DB_DIALECT,
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
 const User = UserModel(sequelize, Sequelize)
