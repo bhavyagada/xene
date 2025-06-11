0f8803b32a247defd7b4f5f72d5b87763de371d6
Bhavya Gada <bhavyasgada@gmail.com>
2025-06-10 19:03:08 -0700

    add port option to sequelize connection

--- server/sequelize.js
+++ server/sequelize.js
@@ -6,6 +6,7 @@ const UserPuzzlesModel = require('../models/user_puzzles')
 
 const sequelize = new Sequelize({
   host: process.env.DB_URL,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
